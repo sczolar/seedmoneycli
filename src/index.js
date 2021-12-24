d2f3f59b860de2d50739f0639da498691391e1a5
sczolar <sczolar@gmail.com>
2021-12-24 19:11:25 +0530

    v1.1

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import store from "./redux/store";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Loading } from "./pages/loading/loading";
 
 const App = React.lazy(() => import("./App"));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={<Loading />}>
+    <Suspense fallback={"loading ..."}>
       <BrowserRouter>
         <Provider store={store}>
           <App />
--- src/pages/loading/loading.js
+++
@@ -1,9 +0,0 @@
-import "./loading.css";
-export const Loading = () => {
-  return (
-    <div class="cir">
-      YoungStorage
-      <span></span>
-    </div>
-  );
-};
